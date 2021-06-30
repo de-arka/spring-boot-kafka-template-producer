efdf91b77b55485b06bf5a0c870cdc86a6df6a9b
Arka De <de_arka@yahoo.com>
2021-06-30 22:02:20 +0530

    Initial Draft

---
+++ src/main/java/com/refresher/kafka/template/producer/SpringBootKafkaTemplateProducerApplication.java
@@ -0,0 +1,13 @@
+package com.refresher.kafka.template.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaTemplateProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaTemplateProducerApplication.class, args);
+    }
+
+}
